60f49be7234ca6dfaded0fb45dea807a09d3a635
Diyorbek-Java <diorbeckturdiev@gmail.com>
2025-03-10 21:55:28 +0500

    corse error fix

--- configs/settings.py
+++ configs/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS Configuration
-CORS_ALLOW_ALL_ORIGINS = False  # Set to True to allow all
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
