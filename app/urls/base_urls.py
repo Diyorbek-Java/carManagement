d1a3d8834bffde3c198ac2320b98693d65c85d40
Diyorbek-Java <diorbeckturdiev@gmail.com>
2025-03-12 20:59:13 +0500

    other urls

--- app/urls/base_urls.py
+++ app/urls/base_urls.py
@@ -5,14 +5,16 @@ from rest_framework.routers import DefaultRouter
 
 from ..view.branch import BranchViewSet
 from ..view.cars import CarViewSet
-
+from users.views import home
 app_name = "base"
 router = DefaultRouter()
 
 router.register("branchs", BranchViewSet, basename="Branches")
 router.register("cars",CarViewSet,basename="Cars")
 
-urlpatterns = []
+urlpatterns = [
+    path("herllo",home,name="second hoem")
+]
 
 urlpatterns += (router.urls 
     ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- configs/urls.py
+++ configs/urls.py
@@ -35,7 +35,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
-    # path("api/v1/", include("app.urls.base-urls")),
+    path("api/v1/", include("app.urls.base_urls")),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("api/v1/token/", user_login, ),
     path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
